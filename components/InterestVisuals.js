f71dfb8feafe99d43bd321243d9fd2c8cb58ec90
Ephraim Becker <emb16@outlook.com>
2026-06-15 17:03:22 -0400

    Update SVG image source to include versioning and adjust train group position in SVG

--- components/InterestVisuals.js
+++ components/InterestVisuals.js
@@ -7,7 +7,7 @@ export default function InterestVisuals({ svgType }) {
 
   return (
     <img
-      src={`${SVG_BASE}${svgType}.svg`}
+      src={`${SVG_BASE}${svgType}.svg?v=3`}
       alt={svgType}
       width="100%"
       style={{ display: 'block', borderRadius: '8px' }}
